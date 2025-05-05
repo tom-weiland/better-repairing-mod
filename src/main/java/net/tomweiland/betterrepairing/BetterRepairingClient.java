e0b8918882abc02857e1740eeab542fe91001e36
Tom Weiland <tomdweiland@gmail.com>
2025-05-05 15:43:20 -0700

    Add initial mod files

---
+++ src/main/java/net/tomweiland/betterrepairing/BetterRepairingClient.java
@@ -0,0 +1,12 @@
+package net.tomweiland.betterrepairing;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BetterRepairingClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+        
+    }
+    
+}
---
+++ src/main/java/net/tomweiland/betterrepairing/BetterRepairingDataGenerator.java
@@ -0,0 +1,11 @@
+package net.tomweiland.betterrepairing;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class BetterRepairingDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
